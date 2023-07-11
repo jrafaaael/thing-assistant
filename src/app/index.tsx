63d36e3f953d7718f70e67e9c2d44b28ca0ecffd
jrafaaael <jrmd070102@gmail.com>
2023-07-11 17:35:10 -0400

    feat: pick `.pdf` documents

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,30 @@
-import { View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+
 import { Text } from "@/components/text";
 
 export default function Home() {
+  const handleFilePicker = async () => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type: "application/pdf",
+    });
+
+    console.log(result);
+  };
+
   return (
-    <View>
-      <Text>alv</Text>
+    <View style={styles.wrapper}>
+      <Pressable onPressIn={handleFilePicker}>
+        <Text>Upload file</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
