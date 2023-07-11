81301f668148344d3780affbe177d837151e5aa1
jrafaaael <jrmd070102@gmail.com>
2023-07-10 20:23:52 -0400

    feat: add `Inter-Bold` font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,6 +9,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     "Inter-Regular": require("../../assets/fonts/Inter-Regular.ttf"),
+    "Inter-Bold": require("../../assets/fonts/Inter-Bold.ttf"),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
