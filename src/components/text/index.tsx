47b1db9f86f72d52cd0d8b3a8b382b6878e4b8c3
jrafaaael <jrmd070102@gmail.com>
2023-07-10 20:24:34 -0400

    feat: add `Text` component

---
+++ src/components/text/index.tsx
@@ -0,0 +1,31 @@
+import { Text as RNText, TextProps } from "react-native";
+
+import { FONT_SIZE } from "@/styles";
+
+import { styles } from "./styles";
+
+interface BaseProps extends TextProps {
+  size?: keyof typeof FONT_SIZE;
+  isTitle?: boolean;
+}
+
+export function Text({
+  children,
+  size = "base",
+  isTitle,
+  ...props
+}: BaseProps) {
+  return (
+    <RNText
+      {...props}
+      style={[
+        styles.base,
+        { fontSize: FONT_SIZE[size] },
+        isTitle && styles.title,
+        props.style,
+      ]}
+    >
+      {children}
+    </RNText>
+  );
+}
---
+++ src/components/text/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "@/styles";
+
+export const styles = StyleSheet.create({
+  base: {
+    color: COLORS.neutral[50],
+    fontFamily: "Inter-Regular",
+  },
+  title: {
+    fontFamily: "Inter-Bold",
+  },
+});
