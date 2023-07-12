b8ce88c4129a1425de86da3a2a56dae754b2dc73
jrafaaael <jrmd070102@gmail.com>
2023-07-11 20:39:03 -0400

    feat: create `supabase` client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_KEY = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+  throw new Error("SUPABASE CREDENTIALS NEEDED");
+}
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
