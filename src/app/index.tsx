2e79c5e7fb6f5baa736d15bf8e1e31243297c9ea
jrafaaael <jrmd070102@gmail.com>
2023-07-11 21:37:55 -0400

    feat: upload file to supabase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,54 @@
+import { useState } from "react";
 import { Pressable, StyleSheet, View } from "react-native";
 import * as DocumentPicker from "expo-document-picker";
 
 import { Text } from "@/components/text";
+import { supabase } from "@/lib/supabase";
 
 export default function Home() {
-  const handleFilePicker = async () => {
+  const [file, setFile] = useState<DocumentPicker.DocumentPickerResult | null>(
+    null
+  );
+
+  const handlePickFile = async () => {
     const result = await DocumentPicker.getDocumentAsync({
       type: "application/pdf",
     });
 
-    console.log(result);
+    if (result.canceled) {
+      return;
+    }
+
+    setFile(result);
+  };
+
+  const handleUploadFile = async () => {
+    if (!file) {
+      return;
+    }
+
+    const fileBlob = {
+      uri: file.assets?.[0].uri,
+      name: file.assets?.[0].name,
+      type: file.assets?.[0].mimeType,
+    } as unknown as Blob;
+    const body = new FormData();
+
+    body.append("file", fileBlob);
+
+    const { data, error } = await supabase.storage
+      .from("pdf")
+      .upload("test.pdf", body);
+
+    console.log({ data, error });
   };
 
   return (
     <View style={styles.wrapper}>
-      <Pressable onPressIn={handleFilePicker}>
+      <Pressable onPressIn={handlePickFile}>
+        <Text>Pick file</Text>
+      </Pressable>
+      <Pressable onPressIn={handleUploadFile}>
         <Text>Upload file</Text>
       </Pressable>
     </View>
