7445230777521f7f85557ae3a64a3af331a968c6
jrafaaael <jrmd070102@gmail.com>
2023-08-06 16:28:57 -0400

    feat: read pdf

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -5,13 +5,20 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 
-@Controller('chat')
+@Controller('chat') 
 export class ChatController {
   @Post('upload-resource')
   @UseInterceptors(FileInterceptor('file'))
-  upload(@UploadedFile() file: Express.Multer.File) {
+  async upload(@UploadedFile() file: Express.Multer.File) {
+    console.time('upload')
     console.log(file);
+    const blob = new Blob([file.buffer]);
+    const loader = new PDFLoader(blob);
+    const docs = await loader.load();
+    console.log(docs);
+    console.timeEnd('upload');
     return { msg: 'ok' };
   }
 }
