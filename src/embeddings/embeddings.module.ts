d35088f0823a0a554ce99c6cb92c77232851231c
jrafaaael <jrmd070102@gmail.com>
2023-08-10 17:25:56 -0400

    fix: remove unused code

--- src/embeddings/embeddings.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmbeddingsController } from './embeddings.controller';
-
-@Module({
-  controllers: [EmbeddingsController],
-})
-export class EmbeddingsModule {}
