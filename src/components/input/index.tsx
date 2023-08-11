d634ea3ddc0e3385de66a3c1d892cde0f1193ab4
jrafaaael <jrmd070102@gmail.com>
2023-08-11 12:47:51 -0400

    feat: make `Input` component customizable

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -3,10 +3,10 @@ import { TextInput, TextInputProps } from "react-native";
 import { COLORS } from "@/styles";
 import { styles } from "./style";
 
-export function Input({ ...props }: TextInputProps) {
+export function Input({ style, ...props }: TextInputProps) {
   return (
     <TextInput
-      style={styles.base}
+      style={[styles.base, style ?? {}]}
       placeholderTextColor={COLORS.neutral[500]}
       {...props}
     />
