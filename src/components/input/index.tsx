31f8be68904a09f0b7bcc0cd355ac9b7a44b00a2
jrafaaael <jrmd070102@gmail.com>
2023-08-15 17:16:48 -0400

    fix: auto grow height in `Input` component

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,14 +1,23 @@
-import { TextInput, TextInputProps } from "react-native";
+import { useState } from "react";
+import { TextInput, TextInputProps, useWindowDimensions } from "react-native";
 
 import { COLORS } from "@/styles";
 import { styles } from "./styles";
 
 export function Input({ style, ...props }: TextInputProps) {
+  const [height, setHeight] = useState<number | null>(null);
+  const { height: windowHeight } = useWindowDimensions();
+  const { maxHeight } = style ?? { maxHeight: windowHeight };
+
   return (
     <TextInput
-      style={[styles.base, style ?? {}]}
+      style={[styles.base, props.multiline ? { height } : {}, style ?? {}]}
       placeholderTextColor={COLORS.neutral[500]}
       {...props}
+      onContentSizeChange={(e) => {
+        const height = Math.min(e.nativeEvent.contentSize.height, maxHeight);
+        setHeight(height);
+      }}
     />
   );
 }
