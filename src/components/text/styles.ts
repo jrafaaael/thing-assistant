02b82742af5176de5cada5939aba77bf008b1e0e
jrafaaael <jrmd070102@gmail.com>
2023-08-17 09:59:49 -0400

    fix: vertical text alignment

--- src/components/text/styles.ts
+++ src/components/text/styles.ts
@@ -5,6 +5,8 @@ export const styles = StyleSheet.create({
   base: {
     color: COLORS.zinc[50],
     fontFamily: "Inter-Regular",
+    verticalAlign: "bottom",
+    textAlignVertical: "bottom",
   },
   title: {
     fontFamily: "Inter-Bold",
