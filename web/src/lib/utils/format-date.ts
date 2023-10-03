c986645e19f0f11fdbed19e177b8621961396242
jrafaaael <jrmd070102@gmail.com>
2023-10-03 12:28:58 -0400

    style(web): format

--- web/src/lib/utils/format-date.ts
+++ web/src/lib/utils/format-date.ts
@@ -1,6 +1,6 @@
-import { isToday } from "./is-today";
-import { isYesterday } from "./is-yesterday";
-import { isDateInCurrentWeek } from "./is-date-in-current-week";
+import { isToday } from './is-today';
+import { isYesterday } from './is-yesterday';
+import { isDateInCurrentWeek } from './is-date-in-current-week';
 
 const WEEKDAYS = ['Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.'];
 
