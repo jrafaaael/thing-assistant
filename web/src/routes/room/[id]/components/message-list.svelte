b7a35e9f2997ec49618ac678cb9e92859ad8d87c
jrafaaael <jrmd070102@gmail.com>
2023-10-05 22:56:28 -0400

    fix(web): overflow before message load

--- web/src/routes/room/[id]/components/message-list.svelte
+++ web/src/routes/room/[id]/components/message-list.svelte
@@ -62,7 +62,7 @@
 		root="parent"
 		onIntersecting={() => $query.hasNextPage && $query.fetchNextPage()}
 	/>
-	<ul class="w-full max-w-3xl min-h-full mx-auto mt-auto px-6 flex flex-col-reverse gap-6">
+	<ul class="w-full max-w-3xl mx-auto mt-auto px-6 flex flex-col-reverse gap-6">
 		{#if $query.isSuccess}
 			{#each messages as message}
 				<li class="flex flex-col gap-1 {message.isFromAi ? 'items-start' : 'items-end'}">
