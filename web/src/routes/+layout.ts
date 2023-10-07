f60e5bf9cc79e8d287a70b67fd2184e673c6026d
jrafaaael <jrmd070102@gmail.com>
2023-10-07 13:04:53 -0400

    refactor(web): extract `Room` type

--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -1,16 +1,9 @@
 import { PUBLIC_API_URL } from '$env/static/public';
-
-interface Room {
-	id: string;
-	name: string;
-	updatedAt: Date;
-	isFromAi: boolean | null;
-	lastMessageContent: string | null;
-}
+import type { RoomWithMessage } from '$lib/types/room.js';
 
 export async function load({ fetch, depends }) {
 	const res = await fetch(`${PUBLIC_API_URL}/rooms`);
-	const data: Room[] = await res.json();
+	const data: RoomWithMessage[] = await res.json();
 
 	depends('layout:rooms');
 
