d57c6a385e6c1440682523a84411c5d452fd1fdb
jrafaaael <jrmd070102@gmail.com>
2023-10-11 11:47:19 -0400

    refactor(web): extract `Room` component

--- web/src/routes/layout/components/room-list.svelte
+++ web/src/routes/layout/components/room-list.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import Link from '$lib/components/link.svelte';
 	import type { RoomWithMessage } from '$lib/types/room';
-	import { formatDate } from '$lib/utils/format-date';
 	import { uploadQueue } from '../store/upload-queue.store';
 	import QueuedRoom from './queued-room.svelte';
+	import Room from './room.svelte';
 
 	export let rooms: RoomWithMessage[];
 </script>
@@ -13,27 +12,6 @@
 		<QueuedRoom name={queuedRoom.name} />
 	{/each}
 	{#each rooms as room (room.id)}
-		<li>
-			<Link
-				class="p-4 border-b-2 border-x-4 border-transparent border-b-white/10 flex flex-col gap-1 hover:bg-neutral-800 [&.active]:bg-neutral-800 [&.active]:border-l-orange-hard"
-				href="/room/{room.id}"
-			>
-				<div class="flex justify-between items-end gap-12">
-					<h6 class="line-clamp-1 break-all flex-1">
-						{room.name}
-					</h6>
-					<time
-						class="text-sm text-neutral-400"
-						datetime={new Date(room.updatedAt).toLocaleString()}
-					>
-						{formatDate(new Date(room.updatedAt))}
-					</time>
-				</div>
-				<p class="text-sm text-neutral-400 line-clamp-1">
-					<span class="font-bold">{room.isFromAi ? 'Cohere' : 'You'}: </span>
-					<span>{room.lastMessageContent}</span>
-				</p>
-			</Link>
-		</li>
+		<Room {...room} />
 	{/each}
 </ul>
