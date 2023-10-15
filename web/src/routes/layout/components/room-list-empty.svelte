98c5a2a7832789379dc983c2f2cddde19c9aaffd
jrafaaael <jrmd070102@gmail.com>
2023-10-15 15:57:09 -0400

    fix(web): empty state when there are not rooms or files to upload

--- web/src/routes/layout/components/room-list-empty.svelte
+++ web/src/routes/layout/components/room-list-empty.svelte
@@ -1,21 +1,29 @@
-<script>
+<script lang="ts">
+	const styles = [
+		{ scale: 0.84, translateY: '30px' },
+		{ scale: 0.92, translateY: '15px' },
+		{ scale: 1, translateY: '0px' }
+	];
 </script>
 
 <div class="h-full px-4 flex flex-col justify-center items-center gap-4">
-	<div class="w-full mb-10 flex flex-col gap-4">
-		<div
-			class="w-full h-16 px-4 bg-gradient-to-b from-white/5 to-white/[0.025] border-t-2 border-t-white/10 border-x-white/[0.075] border-b-transparent rounded-xl flex flex-col justify-evenly overflow-hidden"
-		>
-			<div class="flex gap-12">
-				<div class="w-full h-3 bg-gradient-to-r from-white/10 to-neutral-800 rounded-full" />
-				<div class="w-1/4 h-3 bg-gradient-to-r from-white/10 to-neutral-800 rounded-full grow-0" />
+	<div class="w-full flex flex-col gap-4 relative -translate-y-20">
+		{#each styles as { scale, translateY }}
+			<div
+				class="w-full h-16 px-4 bg-gradient-to-b from-neutral-800 via-neutral-800 to-white/5 border-t-2 border-t-white/5 rounded-xl flex flex-col justify-evenly overflow-hidden absolute"
+				style="transform: scale({scale}) translateY(-{translateY});"
+			>
+				<div class="flex gap-12">
+					<div class="w-full h-3 bg-gradient-to-r from-white/10 to-white/5 rounded-full" />
+					<div class="w-1/4 h-3 bg-gradient-to-r from-white/10 to-white/5 rounded-full" />
+				</div>
+				<div class="w-full h-3 bg-gradient-to-r from-white/10 to-white/5 rounded-full" />
 			</div>
-			<div class="w-full h-3 bg-gradient-to-r from-white/10 to-neutral-800 rounded-full" />
-		</div>
+		{/each}
 	</div>
 	<h2 class="text-2xl text-center font-mono font-bold">You don't have any message yet!</h2>
 	<p class="text-sm text-center">
 		Drag'n drop a file here or click in <span class="font-bold">"New chat"</span> button to upload a
-		file and start chating with it!
+		PDF file and start chating with it!
 	</p>
 </div>
--- web/src/routes/layout/components/room-list.svelte
+++ web/src/routes/layout/components/room-list.svelte
@@ -3,15 +3,20 @@
 	import { uploadQueue } from '../store/upload-queue.store';
 	import QueuedRoom from './queued-room.svelte';
 	import Room from './room.svelte';
+	import RoomListEmpty from './room-list-empty.svelte';
 
 	export let rooms: RoomWithMessage[];
 </script>
 
-<ul class="flex flex-col">
-	{#each $uploadQueue as queuedRoom}
-		<QueuedRoom name={queuedRoom.name} />
-	{/each}
-	{#each rooms as room (room.id)}
-		<Room {...room} />
-	{/each}
-</ul>
+{#if data.rooms.length > 0 || $uploadQueue.length > 0}
+	<ul class="flex flex-col">
+		{#each $uploadQueue as queuedRoom}
+			<QueuedRoom name={queuedRoom.name} />
+		{/each}
+		{#each data.rooms as room (room.id)}
+			<Room {...room} />
+		{/each}
+	</ul>
+{:else}
+	<RoomListEmpty />
+{/if}
