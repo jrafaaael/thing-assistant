dc4373eb0ade7c0730f5efd1d28f802d9adf1471
jrafaaael <jrmd070102@gmail.com>
2023-10-15 17:38:53 -0400

    feat(web): dispatch `change` event when new files are droped

--- web/src/lib/components/dnd.svelte
+++ web/src/lib/components/dnd.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
 	export let files: File[] = [];
 	export let isDrag = false;
 	export let accept: string | null = null;
+
+	const dispatcher = createEventDispatcher();
 
 	function handleDrop(e: DragEvent & { currentTarget: EventTarget & HTMLDivElement }) {
 		isDrag = false;
 		const dt = e.dataTransfer!;
 		files = accept ? [...dt.files].filter(({ type }) => type === accept) : [...dt.files];
 
-		console.log(files);
+		if (files.length > 0) {
+			dispatcher('change');
+		}
 	}
 </script>
 
