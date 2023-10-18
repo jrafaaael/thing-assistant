022c0a1095f167203dabba1940cd065c1def518b
jrafaaael <jrmd070102@gmail.com>
2023-10-18 09:39:43 -0400

    fix(web): set infinity timeout

--- web/src/lib/libs/ky.ts
+++ web/src/lib/libs/ky.ts
@@ -2,5 +2,6 @@ import { PUBLIC_API_URL } from '$env/static/public';
 import lib from 'ky';
 
 export const ky = lib.create({
-	prefixUrl: PUBLIC_API_URL ?? ''
+	prefixUrl: PUBLIC_API_URL ?? '',
+	timeout: false
 });
